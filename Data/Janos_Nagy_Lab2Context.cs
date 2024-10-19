946a79d51804153a41a3c6a38445c8a101497e99
Nagy Janos <105793197+NaJaBox@users.noreply.github.com>
2024-10-19 17:00:42 +0200

    Add project files.

---
+++ Data/Janos_Nagy_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Janos_Nagy_Lab2.Models;
+
+namespace Janos_Nagy_Lab2.Data
+{
+    public class Janos_Nagy_Lab2Context : DbContext
+    {
+        public Janos_Nagy_Lab2Context (DbContextOptions<Janos_Nagy_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Janos_Nagy_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Janos_Nagy_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+        public DbSet<Janos_Nagy_Lab2.Models.Author> Author { get; set; } = default!;
+    }
+}
---
+++ Models/Author.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Janos_Nagy_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        [Display(Name = "Author")]
+        public string AuthorName { get; set; }
+        public ICollection<Book>? Books { get; set; } //navigation property
+    }
+}
