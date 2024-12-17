fcedbee0406cd74022ef464b9dfb1b9a5aabe67c
Nagy Janos <105793197+NaJaBox@users.noreply.github.com>
2024-12-17 15:00:50 +0100

    * added new branch - Laborator6

--- Models/Book.cs
+++ Models/Book.cs
@@ -7,8 +7,12 @@ namespace Janos_Nagy_Lab2.Models
     public class Book
     {
         public int ID { get; set; }
+
+        [Required(ErrorMessage = "The title is required.")]
+        [StringLength(150, MinimumLength = 3, ErrorMessage = "Titlul trebuie sa fie intre 3 si 150 caractere.")]
         [Display(Name = "Book Title")]
         public string Title { get; set; }
+
         [Column(TypeName = "decimal(6, 2)")]
         public decimal Price { get; set; }
 
